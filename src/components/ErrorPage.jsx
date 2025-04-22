77a598c26a624393843f66d5d96978be3a974af4
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-04-22 21:48:37 +0800

    add styles to error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,11 @@
+import styles from "../styles/ErrorPage.module.css";
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <div>
-        <h2>OOPS! THIS ROUTE DOES NOT EXIST</h2>
+        <div className={styles.base}>
+        <h2>Oops! This Route Does Not exist</h2>
+        <Link to={"/"}>Return to Home</Link>
         </div>
     )
 }
